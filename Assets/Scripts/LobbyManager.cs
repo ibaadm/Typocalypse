cfe32a91f1ac52c50c94e9303021565e2f6a7f4c
Ibaad Muhammad <ibaad.muhammad01@gmail.com>
2025-09-23 01:51:08 +0100

    feat: finish lobby and setup relay
    Mostly finished, a relay is now established on lobby creation/joining. There are some errors on scene loading that requires fixing

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
+using System.Collections.Generic;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Authentication;
 using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
 using UnityEngine.InputSystem;
 using Unity.Netcode;
+using Unity.Services.Relay.Models;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine.SceneManagement;
 
 public class LobbyManager : NetworkBehaviour {
 
     Lobby hostLobby;
+    Lobby joinedLobby;
     float lobbyHearbeatTimer = 15f;
     float updateLobbyTimer = 1.1f;
     public string lobbyCodeInput = "";
     public static LobbyManager instance;
     LobbyEventCallbacks callBacks = new LobbyEventCallbacks();
     bool shouldBeHost;
+    bool relayCreated = false;
+    bool inDuelScene = false;
 
     void Awake() {
 
@@ -61,6 +69,9 @@ public class LobbyManager : NetworkBehaviour {
 
             CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions {
                 IsPrivate = true,
+                Data = new Dictionary<string, DataObject> {
+                    { "KEY_START_GAME", new DataObject(DataObject.VisibilityOptions.Member, "0")}
+                }
             };
 
             hostLobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers, createLobbyOptions);
@@ -86,14 +97,30 @@ public class LobbyManager : NetworkBehaviour {
         }
     }
 
-        async void UpdateLobby() {
+    async void UpdateLobby() {
         if (updateLobbyTimer > 0) {
             updateLobbyTimer -= Time.unscaledDeltaTime;
         }
         else {
             updateLobbyTimer = 1.1f;
             try {
-                hostLobby = await LobbyService.Instance.GetLobbyAsync(hostLobby.Id);
+                if (shouldBeHost) {
+                    hostLobby = await LobbyService.Instance.GetLobbyAsync(hostLobby.Id);
+                    if (hostLobby.Players.Count == 2 && !relayCreated) {
+                        Debug.Log("Client joined");
+                        CreateRelay();
+                    }
+                }
+                else {
+                    joinedLobby = await LobbyService.Instance.GetLobbyAsync(joinedLobby.Id);
+                    if (joinedLobby.Data["KEY_START_GAME"].Value != "0" && !relayCreated) {
+                        JoinRelay(joinedLobby.Data["KEY_START_GAME"].Value);
+                    }
+                    if (IsClient && !inDuelScene) {
+                        LoadDuelSceneServerRpc();
+                        inDuelScene = true;
+                    }
+                }
             }
             catch {
             }
@@ -103,7 +130,7 @@ public class LobbyManager : NetworkBehaviour {
     async void JoinLobby(string lobbyCode) {
 
         try {
-            hostLobby = await LobbyService.Instance.JoinLobbyByCodeAsync(lobbyCode);
+            joinedLobby = await LobbyService.Instance.JoinLobbyByCodeAsync(lobbyCode);
             Debug.Log("should be joined");
             shouldBeHost = false;
         }
@@ -120,5 +147,59 @@ public class LobbyManager : NetworkBehaviour {
         catch {
             Debug.Log("error leaving lobby");
         }
+        // need to leave relay too? should change relayCreated to false in that case
+    }
+
+    async void CreateRelay() {
+        try {
+            relayCreated = true;
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            transport.SetRelayServerData(
+                allocation.RelayServer.IpV4,
+                (ushort)allocation.RelayServer.Port,
+                allocation.AllocationIdBytes,
+                allocation.Key,
+                allocation.ConnectionData
+            );
+            hostLobby = await LobbyService.Instance.UpdateLobbyAsync(hostLobby.Id, new UpdateLobbyOptions {
+                Data = new Dictionary<string, DataObject> {
+                    { "KEY_START_GAME", new DataObject(DataObject.VisibilityOptions.Member, joinCode)}
+                }
+            });
+            NetworkManager.Singleton.StartHost();
+        }
+        catch (RelayServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    async void JoinRelay(string joinCode) {
+        try {
+            relayCreated = true;
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            transport.SetRelayServerData(
+                joinAllocation.RelayServer.IpV4,
+                (ushort)joinAllocation.RelayServer.Port,
+                joinAllocation.AllocationIdBytes,
+                joinAllocation.Key,
+                joinAllocation.ConnectionData,
+                joinAllocation.HostConnectionData
+            );
+
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (RelayServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void LoadDuelSceneServerRpc() {
+        NetworkManager.Singleton.SceneManager.LoadScene("DuelScene", LoadSceneMode.Single);
+        Debug.Log("switch scenes called");
     }
 }
