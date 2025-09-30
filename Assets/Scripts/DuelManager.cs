b29b466a2a0f6cf31ec5199df67f41cd79c2f5ca
Ibaad Muhammad <ibaad.muhammad01@gmail.com>
2025-10-01 00:13:35 +0100

    feat: Add ready up UI
    Players are now told to press space to ready up and can see who has readied up.

--- Assets/Scripts/DuelManager.cs
+++ Assets/Scripts/DuelManager.cs
@@ -2,11 +2,15 @@ using Unity.Netcode;
 using UnityEngine;
 using System.Collections;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class DuelManager : NetworkBehaviour {
 
+    [SerializeField] TextMeshProUGUI blueReadyText;
+    [SerializeField] TextMeshProUGUI redReadyText;
     private NetworkVariable<int> playersReady = new NetworkVariable<int>(0);
     private bool ready = false;
+
 
     void Start() {
         if (LobbyManager.instance.shouldBeHost == true) {
@@ -33,6 +37,20 @@ public class DuelManager : NetworkBehaviour {
             if (Keyboard.current.spaceKey.wasPressedThisFrame && !ready) {
                 ready = true;
                 IncreasePlayersReadyServerRpc();
+                if (IsHost) {
+                    blueReadyText.text = "Ready";
+                }
+                else {
+                    redReadyText.text = "Ready";
+                }
+            }
+            if (playersReady.Value == 1 && !ready) {
+                if (IsHost) {
+                    redReadyText.text = "Ready";
+                }
+                else {
+                    blueReadyText.text = "Ready";
+                }
             }
             yield return null;
         }
