dd9e30e3dbf5a8cb8dcb0e31bb053bbb12f081e4
Ibaad Muhammad <ibaad.muhammad01@gmail.com>
2026-01-30 01:17:56 +0000

    feat: add UI indicating player replaying status

--- Assets/Scripts/DeathManager.cs
+++ Assets/Scripts/DeathManager.cs
@@ -11,21 +11,35 @@ public class DeathManager : NetworkBehaviour {
     [SerializeField] private TextMeshProUGUI playerBlueScore;
     [SerializeField] private TextMeshProUGUI playerRedTime;
     [SerializeField] private TextMeshProUGUI playerRedScore;
+    [SerializeField] private TextMeshProUGUI playerBlueTick;
+    [SerializeField] private TextMeshProUGUI playerRedTick;
+    [SerializeField] private Color playerBlueTicked;
+    [SerializeField] private Color playerBlueUnticked;
+    [SerializeField] private Color playerRedTicked;
+    [SerializeField] private Color playerRedUnticked;
+    private bool wasHost = false;
+    private bool wasClient = false;
+
+
     private NetworkVariable<int> replayingPlayers = new NetworkVariable<int>(0);
 
     void Start() {
         playerBlueTime.text = playerBlueScore.text = playerRedScore.text = playerRedTime.text = "";
+        playerBlueTick.text = playerRedTick.text = "";
     }
 
     public void OnPanelEnable() {
         if (HUDManager.timeText.text != "0:00") {
             if (IsHost) {
                 DisplayDuelScoresClientRpc(HUDManager.timeText.text, HUDManager.score);
+                wasHost = true;
             }
             else if (IsClient) {
                 DisplayDuelScoresServerRpc(HUDManager.timeText.text, HUDManager.score);
             }
         }
+        playerBlueTick.text = playerRedTick.text = "<sprite index=0 tint=1>";
+        wasClient = true;
     }
 
     [ServerRpc(RequireOwnership = false)]
@@ -49,11 +63,28 @@ public class DeathManager : NetworkBehaviour {
     public void Replay() {
 
         AudioManager.instance.PlayButtonPressSFX();
-
-        if (IsClient) {
+        if (wasClient) {
             if (!LobbyManager.instance.isRetrying) {
                 LobbyManager.instance.isRetrying = true;
-                IncreasePlayersRetryingServerRpc();
+                if (wasHost) {
+                    IncreasePlayersRetryingServerRpc(true);
+                    ChangeTickLocal(true, true);
+                }
+                else {
+                    IncreasePlayersRetryingServerRpc(false);
+                    ChangeTickLocal(false, true);
+                }
+            }
+            else {
+                LobbyManager.instance.isRetrying = false;
+                if (wasHost) {
+                    DecreasePlayersRetryingServerRpc(true);
+                    ChangeTickLocal(true, false);
+                }
+                else {
+                    DecreasePlayersRetryingServerRpc(false);
+                    ChangeTickLocal(false, false);
+                }
             }
             return;
         }
@@ -61,17 +92,32 @@ public class DeathManager : NetworkBehaviour {
         SceneManager.LoadScene("SingleScene");
     }
     [ServerRpc (RequireOwnership = false)]
-    void IncreasePlayersRetryingServerRpc() {
+    void IncreasePlayersRetryingServerRpc(bool blue) {
         replayingPlayers.Value++;
+        
         if (replayingPlayers.Value >= 2) {
             LobbyManager.instance.DisconnectServerRpc();
+        }
+
+        if (blue) {
+            ChangeTickClientRpc(true, true);
+        }
+        else {
+            ChangeTickClientRpc(false, true);
         }
     }
 
     public void Menu() {
 
-        if (IsClient) {
-            DecreasePlayersRetryingServerRpc();
+        if (wasClient) {
+            if (wasHost) {
+                DecreasePlayersRetryingServerRpc(true);
+                ChangeTickLocal(true, false);
+            }
+            else {
+                DecreasePlayersRetryingServerRpc(false);
+                ChangeTickLocal(false, false);
+            }
             LobbyManager.instance.DisconnectServerRpc();
             LobbyManager.instance.MenuCleanup();
             return;
@@ -82,7 +128,37 @@ public class DeathManager : NetworkBehaviour {
     }
 
     [ServerRpc (RequireOwnership = false)]
-    void DecreasePlayersRetryingServerRpc() {
+    void DecreasePlayersRetryingServerRpc(bool blue) {
         replayingPlayers.Value--;
+        if (blue) {
+            ChangeTickClientRpc(true, false);
+        }
+        else {
+            ChangeTickClientRpc(false, false);
+        }
+    }
+
+    [ClientRpc]
+    void ChangeTickClientRpc(bool blue, bool ticked) {
+        ChangeTickLocal(blue, ticked);
+    }
+
+        void ChangeTickLocal(bool blue, bool ticked) {
+        if (blue) {
+            if (ticked) {
+                playerBlueTick.color = playerBlueTicked;
+            }
+            else {
+                playerBlueTick.color = playerBlueUnticked;
+            }
+        }
+        else {
+            if (ticked) {
+                playerRedTick.color = playerRedTicked;
+            }
+            else {
+                playerRedTick.color = playerRedUnticked;
+            }
+        }
     }
 }
